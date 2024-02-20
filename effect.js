4fc4de182f03c44f1ed71e3a861efc480056f913
rubenaa01 <102738825+rubenaa01@users.noreply.github.com>
2024-02-20 20:52:06 +0100

    Update effect.js

--- effect.js
+++ effect.js
@@ -178,8 +178,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1500);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					
 					$('.foto').addClass('foto-come');
+					$('.foto').removeClass('foto');
 				});
 				
 			}
